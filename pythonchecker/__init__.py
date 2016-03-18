f67dcf43f8868fd97a124e77ec764d01c7d1adbd
molinav <molinav@users.noreply.github.com>
2016-03-18 14:47:26 +0100

    Add description comments

--- pythonchecker/__init__.py
+++ pythonchecker/__init__.py
@@ -1,26 +1,28 @@
+"""Gedit Python Checker Plugin"""
+
 from . main.controller import CheckerController
 
 
-class PythonCheckerPluginAppActivatable(object):
-    
-    def do_activate(self):
-        pass
-    
-    def do_deactivate(self):
-        pass
+class WindowActivatable(CheckerController):
+    """Plugin class."""
 
-
-class PythonCheckerWindowActivatable(CheckerController):
-    
     def do_activate(self):
+        """Run in order to enable the plugin."""
+
         self.enable()
-    
+
     def do_deactivate(self):
+        """Run in order to disable the plugin."""
+
         self.disable()
-    
+
     def do_update_state(self):
+        """Run in case of state update."""
+
         pass
 
     def do_create_configure_widget(self):
+        """Return preferences dialog."""
+
         return self.configure()
 
--- pythonchecker/_decorators.py
+++ pythonchecker/_decorators.py
@@ -1,13 +1,21 @@
+"""_decorators.py
+
+Auxiliary decorators for plugin methods.
+"""
+
+
 from threading import Thread
 from gi.repository import GLib
 
 
 def threaded_with_python(func):
+    """Send function into a new thread managed by Python."""
 
     def wrapper(self, *args, **kwargs):
+        """Function wrapper."""
 
         def inner():
-            
+            """Function inside wrapper."""
             return func(self, *args, **kwargs)
 
         thread = Thread(target=inner)
@@ -18,10 +26,13 @@ def threaded_with_python(func):
 
 
 def threaded_with_glib(func):
+    """Send function into a new thread managed by GLib."""
 
     def wrapper(self, *args, **kwargs):
+        """Function wrapper."""
 
         def inner():
+            """Function inside wrapper."""
             return func(self, *args, **kwargs)
 
         GLib.idle_add(inner)
